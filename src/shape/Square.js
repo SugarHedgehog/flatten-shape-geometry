b36b09f5a4adec72f7f14f0a4b89001ec2d480b2
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-03-22 14:18:54 +0300

    more beuty

--- src/shape/Square.js
+++ src/shape/Square.js
@@ -1,9 +1,9 @@
 import Quadrilateral from "./Quadrilateral";
-import { Point, Segment } from '@flatten-js/core';
-import {shiftCoordinate2D} from '../functions/general.js'
+import { Point } from '@flatten-js/core';
+import { shiftCoordinate2D } from '../functions/general.js'
 
-export default class Square extends Quadrilateral{
-    constructor(length){
+export default class Square extends Quadrilateral {
+    constructor(length) {
         super();
         if (length <= 0 || !Number.isFinite(Number(length)))
             throw new TypeError(`Invalid length: Received length is ${JSON.stringify(lengths)}. Please provide three numeric side lengths.`);
@@ -13,32 +13,32 @@ export default class Square extends Quadrilateral{
         this.addFace([this._pointA, this._pointB, this._pointC, this._pointD]);
     }
 
-    #setSides(length){
+    #setSides(length) {
         this._lengthAB = length;
         this._lengthBC = length;
         this._lengthCD = length;
         this._lengthDA = length;
     }
 
-    #setAngles(){
-        this._angleAInRadians = Math.PI/2;
-        this._angleBInRadians = Math.PI/2;
-        this._angleCInRadians = Math.PI/2;
-        this._angleDInRadians = Math.PI/2;
+    #setAngles() {
+        this._angleAInRadians = Math.PI / 2;
+        this._angleBInRadians = Math.PI / 2;
+        this._angleCInRadians = Math.PI / 2;
+        this._angleDInRadians = Math.PI / 2;
     }
 
 
-    #setCoordites(length){
+    #setCoordites(length) {
         const A = new Point(0, 0);
         const B = new Point(length, 0);
         const C = new Point(length, length);
         const D = new Point(0, length);
 
-        [this._pointA, this._pointB, this._pointC, this._pointD] = [A,B,C,D].map((vertex) => shiftCoordinate2D(vertex, new Point(length/2, length/2)));
+        [this._pointA, this._pointB, this._pointC, this._pointD] = [A, B, C, D].map((vertex) => shiftCoordinate2D(vertex, new Point(length / 2, length / 2)));
         this._vertices = [this._pointA, this._pointB, this._pointC, this._pointD];
     }
 
-    get vertices(){
+    get vertices() {
         return this._vertices;
     }
 }
