7a483ceb93a524fe7b69c3ebdfd11d0213394f5c
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-03-25 16:39:17 +0300

    add test for Parallelogram

---
+++ tests/Parallelogram.test.js
@@ -0,0 +1,93 @@
+import Parallelogram from '../src/shape/Parallelogram';
+import { Point} from '@flatten-js/core';
+import degreesToRadians from 'degrees-radians';
+import { shiftCoordinate2D } from '../src/functions/general.js';
+
+describe('Parallelogram', () => {
+    it('should create a parallelogram with given side lengths and angles', () => {
+        const parallelogram = new Parallelogram({ lengthAB: 4, lengthBC: 3 }, { angle: { angleA: 60 }, angleInDegree: true });
+        expect(parallelogram.lengthAB).toBe(4);
+        expect(parallelogram.lengthBC).toBe(3);
+        expect(parallelogram.lengthCD).toBe(4);
+        expect(parallelogram.lengthDA).toBe(3);
+    });
+
+    it('should have correct angles in radians and degrees', () => {
+        const parallelogram = new Parallelogram({ lengthAB: 4, lengthBC: 3 }, { angle: { angleA: 60 }, angleInDegree: true });
+        expect(parallelogram.angleAInRadians).toBeCloseTo(degreesToRadians(60));
+        expect(parallelogram.angleBInRadians).toBeCloseTo(degreesToRadians(120));
+        expect(parallelogram.angleCInRadians).toBeCloseTo(degreesToRadians(60));
+        expect(parallelogram.angleDInRadians).toBeCloseTo(degreesToRadians(120));
+
+        expect(parallelogram.angleAInDegrees).toBeCloseTo(60);
+        expect(parallelogram.angleBInDegrees).toBeCloseTo(120);
+        expect(parallelogram.angleCInDegrees).toBeCloseTo(60);
+        expect(parallelogram.angleDInDegrees).toBeCloseTo(120);
+    });
+
+    it('should have correct vertex coordinates', () => {
+        const parallelogram = new Parallelogram({ lengthAB: 4, lengthBC: 3 }, { angle: { angleA: 60 }, angleInDegree: true });
+        const x = 3 * Math.cos(degreesToRadians(60));
+        const y = 3 * Math.sin(degreesToRadians(60));
+
+        const expectedPoints = [
+            new Point(0, 0),
+            new Point(4, 0),
+            new Point(4 + x, y),
+            new Point(x, y)
+        ].map(p => shiftCoordinate2D(p, new Point(2 + x/2, y/2)));
+
+        expectedPoints.forEach((point, index) => {
+            const actualPoint = index === 0 ? parallelogram.pointA :
+                index === 1 ? parallelogram.pointB :
+                    index === 2 ? parallelogram.pointC :
+                        parallelogram.pointD;
+            expect(actualPoint.x).toBeCloseTo(point.x);
+            expect(actualPoint.y).toBeCloseTo(point.y);
+        });
+    });
+
+    it('should correctly calculate perimeter and area', () => {
+        const parallelogram = new Parallelogram({ lengthAB: 4, lengthBC: 3 }, { angle: { angleA: 60 }, angleInDegree: true });
+        expect(parallelogram.perimeter).toBe(14);
+        expect(parallelogram.area()).toBeCloseTo(4 * 3 * Math.sin(degreesToRadians(60)));
+    });
+
+    it('should correctly calculate diagonals when requested', () => {
+        const parallelogram = new Parallelogram({ lengthAB: 4, lengthBC: 3 }, { angle: { angleA: 60 }, angleInDegree: true }, { calculateDiagonals: true });
+        const d1 = Math.sqrt(4**2 + 3**2 + 2*4*3*Math.cos(degreesToRadians(60)));
+        const d2 = Math.sqrt(4**2 + 3**2 - 2*4*3*Math.cos(degreesToRadians(60)));
+
+        expect(parallelogram.lengthDiagonalAC).toBeCloseTo(d1);
+        expect(parallelogram.lengthDiagonalBD).toBeCloseTo(d2);
+
+        const diagonalAC = parallelogram.diagonalAC;
+        expect(diagonalAC.start.x).toBeCloseTo(parallelogram.pointA.x);
+        expect(diagonalAC.start.y).toBeCloseTo(parallelogram.pointA.y);
+        expect(diagonalAC.end.x).toBeCloseTo(parallelogram.pointC.x);
+        expect(diagonalAC.end.y).toBeCloseTo(parallelogram.pointC.y);
+
+        const diagonalBD = parallelogram.diagonalBD;
+        expect(diagonalBD.start.x).toBeCloseTo(parallelogram.pointB.x);
+        expect(diagonalBD.start.y).toBeCloseTo(parallelogram.pointB.y);
+        expect(diagonalBD.end.x).toBeCloseTo(parallelogram.pointD.x);
+        expect(diagonalBD.end.y).toBeCloseTo(parallelogram.pointD.y);
+
+        const intersectionPoint = parallelogram.diagonalIntersectionPoint;
+        expect(intersectionPoint.x).toBeCloseTo(0);
+        expect(intersectionPoint.y).toBeCloseTo(0);
+    });
+
+    it('should throw an exception for invalid lengths', () => {
+        expect(() => new Parallelogram({}, {})).toThrow(/No lengths of the parallelogram are defined/);
+        expect(() => new Parallelogram({ lengthAB: -1, lengthBC: 2 }, {})).toThrow(/Length isnt positive numeric value/);
+        expect(() => new Parallelogram({ lengthAB: "test", lengthBC: 2 }, {})).toThrow(/Length isnt positive numeric value/);
+        expect(() => new Parallelogram({ lengthAB: 2, lengthCD: 3 }, {})).toThrow(/Two parallel sides of a parallelogram are given/);
+    });
+
+    it('should throw an exception for invalid angles', () => {
+        expect(() => new Parallelogram({ lengthAB: 4, lengthBC: 3 }, {})).toThrow(/No angle of the parallelogram is defined/);
+        expect(() => new Parallelogram({ lengthAB: 4, lengthBC: 3 }, { angle: {} })).toThrow(/No angle of the parallelogram is defined/);
+        expect(() => new Parallelogram({ lengthAB: 4, lengthBC: 3 }, { angle: { angleE: 60 } })).toThrow(/Angles arent difined/);
+    });
+});
