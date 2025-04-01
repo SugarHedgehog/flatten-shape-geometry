d5ddc31ad502f9ecf1a4e4fb23ac62551cd5cab5
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-04-01 12:29:29 +0300

    delete _lengths and setSide

--- src/shape/Rectangle.js
+++ src/shape/Rectangle.js
@@ -5,14 +5,13 @@ import { shiftCoordinate2D } from '../functions/general.js'
 export default class Rectangle extends Quadrilateral {
     constructor(lengths = {}, supplementary = {}){
         super();
-        const {width, height} = lengths;
-        if (width <= 0|| height <= 0 || !Number.isFinite(Number(width))|| !Number.isFinite(Number(height)))
-            throw new TypeError(`Invalid lengths: Received lengths are ${JSON.stringify(lengths)}. Please provide two positive numeric side lengths.`);
 
-        this.#setSides(width, height)
+        if (Object.keys(lengths).length !== 2)
+            throw new Error(`There must be exactly two lengths. Received: ${JSON.stringify(lengths)}`);
+
         this.#setAngles();
-        this.#setCoordites(width, height);
-        this.addFace([this._pointA, this._pointB, this._pointC, this._pointD]);
+        this.#setCoordites(lengths);
+        this.addFace(this._vertices);
 
         const {
             calculateDiagonals = false,
@@ -23,13 +22,6 @@ export default class Rectangle extends Quadrilateral {
         }
     }
 
-    #setSides(width, height) {
-        this._lengthAB = width;
-        this._lengthBC = height;
-        this._lengthCD = width;
-        this._lengthDA = height;
-    }
-
     #setAngles() {
         this._angleAInRadians = Math.PI / 2;
         this._angleBInRadians = Math.PI / 2;
@@ -37,7 +29,29 @@ export default class Rectangle extends Quadrilateral {
         this._angleDInRadians = Math.PI / 2;
     }
 
-    #setCoordites(width, height) {
+    #setCoordites(lengths) {
+        let width, height;
+
+        Object.keys(lengths).forEach(key => {
+            const length = lengths[key];
+            if(length > 0 && Number.isFinite(length)){
+                switch(key){
+                    case 'lengthAB':
+                    case 'lengthCD': 
+                        width = length;
+                        break;
+                    case 'lengthBC':
+                    case 'lengthDA':   
+                        height = length;
+                        break;
+                    default:
+                        throw new Error(`Unknown lengths: ${JSON.stringify(lengths)}`);
+                }
+            } else {
+                throw new Error(`The length must be a positive number: ${key}:${length}`);
+            }
+        });
+
         const A = new Point(0, 0);
         const B = new Point(width, 0);
         const C = new Point(width, height);
