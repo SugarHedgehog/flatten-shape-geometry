5fc3568f1fa1fbe7f0fae7850281f9bab5dde10b
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-04-13 17:54:37 +0300

    tangentsFromPoint

--- src/functions/methods_for_circle.js
+++ src/functions/methods_for_circle.js
@@ -1,4 +1,4 @@
-import { Circle, Point, Segment } from '@flatten-js/core';
+import { Circle, Point, Segment, Vector } from '@flatten-js/core';
 import degreesToRadians from 'degrees-radians';
 
 Circle.prototype.pointOnCircle = function (angle, {angleInDegrees = false, counterclockwise = false} = {}) {
@@ -11,7 +11,7 @@ Circle.prototype.pointOnCircle = function (angle, {angleInDegrees = false, count
     if (Math.abs(angle) > 2 * Math.PI) {
         throw new Error(`The angle is greater than 2 pi`);
     }
-    
+
     if(counterclockwise){
         angle *= -1;
     }
@@ -33,7 +33,7 @@ Circle.prototype.diameter = function(angle, {angleInDegrees = false, countercloc
         angleInDegrees,
         counterclockwise
     });
-    
+
     return new Segment(startPoint, endPoint);
 };
 
@@ -44,7 +44,53 @@ Circle.prototype.chordByAngles = function(startAngle, endAngle, {angleInDegrees 
 
     const startPoint = this.pointOnCircle(startAngle, {angleInDegrees, counterclockwise});
     const endPoint = this.pointOnCircle(endAngle, {angleInDegrees, counterclockwise});
-    
+
     return new Segment(startPoint, endPoint);
+};
+
+Circle.prototype.tangentsFromPoint = function(point, {segmentLength = 0} = {}) {
+    if (!(point instanceof Point)) {
+        throw new Error("First argument must be a Point");
+    }
+    if (typeof segmentLength !== 'number' || segmentLength < 0) {
+        throw new Error("segmentLength must be a non-negative number");
+    }
+    
+    const circle = this;
+    const center = circle.center;
+    const radius = circle.r;
+    
+    const distanceToCenter = center.distanceTo(point)[0];
+    if (distanceToCenter <= radius) {
+        throw new Error("Point must be outside the circle");
+    }
+
+    const vecToPoint = new Vector(center, point);
+    const d = vecToPoint.length;
+    
+    const angle = Math.acos(radius / d);
+    
+    // Рассчитываем базовые точки касания
+    const rotatedVec1 = vecToPoint.rotate(angle).normalize();
+    const rotatedVec2 = vecToPoint.rotate(-angle).normalize();
+    
+    let tangentPoint1 = center.translate(rotatedVec1.multiply(radius));
+    let tangentPoint2 = center.translate(rotatedVec2.multiply(radius));
+
+    // Если задана длина отрезка, устанавливаем фиксированную длину
+    if (segmentLength > 0) {
+        // Создаем векторы ОТ центральной точки К точкам касания
+        const vector1 = new Vector(point, tangentPoint1).normalize();
+        const vector2 = new Vector(point, tangentPoint2).normalize();
+        
+        // Устанавливаем новые позиции с фиксированной длиной от центра
+        tangentPoint1 = point.translate(vector1.multiply(segmentLength));
+        tangentPoint2 = point.translate(vector2.multiply(segmentLength));
+    }
+
+    return [
+        new Segment(point, tangentPoint1),
+        new Segment(point, tangentPoint2)
+    ];
 };
 
