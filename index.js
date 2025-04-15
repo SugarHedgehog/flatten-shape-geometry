59489ddb9d8771cf0a71e542c5d64296a64000e0
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-04-15 22:16:39 +0300

    add all classes in index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import { Circle } from '@flatten-js/core';  
+import {Point, Vector, Circle, Line, Ray, Segment, Arc, Box, Polygon, Matrix, PlanarSet} from '@flatten-js/core'; 
 import {isValidTriangle, calculateThirdSideUsingCosineLaw, bisectorIntersection} from './src/functions/general'
 import './src/functions/methods_for_circle';  
 import Angle from './src/shape/Angle';
@@ -11,6 +11,17 @@ import Rhombus from './src/shape/Rhombus';
 import Parallelogram from './src/shape/Parallelogram';
 import Trapezoid from './src/shape/Trapezoid';
 
+window.Point = Point;
+window.Vector = Vector;
+window.Circle = Circle;
+window.Line = Line;
+window.Ray = Ray;
+window.Segment = Segment;
+window.Arc = Arc;
+window.Box = Box;
+window.Polygon = Polygon;
+window.Matrix = Matrix;
+window.PlanarSet = PlanarSet;
 window.isValidTriangle = isValidTriangle;
 window.calculateThirdSideUsingCosineLaw = calculateThirdSideUsingCosineLaw;
 window.bisectorIntersection = bisectorIntersection;
@@ -21,4 +32,4 @@ window.Rectangle = Rectangle;
 window.Rhombus = Rhombus;
 window.Parallelogram = Parallelogram;
 window.Trapezoid = Trapezoid;
-window.Circle = Circle;
+
