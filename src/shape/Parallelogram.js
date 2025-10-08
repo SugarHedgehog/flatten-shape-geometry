81eb8eb614bb168b1e65423269d6319624f5ca76
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-10-08 13:17:41 +0300

    Add support for constructing Parallelogram from four points

--- src/shape/Parallelogram.js
+++ src/shape/Parallelogram.js
@@ -1,29 +1,38 @@
 import Quadrilateral from "./Quadrilateral";
 import { Point, Segment } from '@flatten-js/core';
 import degreesToRadians from 'degrees-radians';
-import { shiftCoordinate2D } from '../functions/general.js'
+import { shiftCoordinate2D, isValidQuadrilateral } from '../functions/general.js'
+import Angle from "./Angle.js";
 
 export default class Parallelogram extends Quadrilateral {
-    constructor({lengths = {}, angles = {}, supplementary = {}} = {}) {
+    constructor({points = [], lengths = {}, angles = {}, supplementary = {}} = {}) {
         super();
-
-        if (Object.keys(lengths).length < 2)
-            throw new Error(`No lengths of the parallelogram are defined. ${JSON.stringify(lengths)}`);
-
-        if(('lengthAB' in lengths || 'lengthBC' in lengths) && ('lengthCD' in lengths || 'lengthDA' in lengths))
-            throw new Error(`Two parallel sides of a parallelogram are given. ${JSON.stringify(lengths)}`);
-
-        if (!angles.angle || typeof angles.angle !== 'object' || Object.keys(angles.angle).length === 0)
-            throw new TypeError(`No angle of the parallelogram is defined. ${JSON.stringify(angles)}`);
-        
-        this.#setAngles(angles);
-        this.#setCoordinates(lengths);
-        this.addFace(this._vertices);
 
         const {
             calculateDiagonals = false,
             calculateHeights = false,
+            shiftCoordinate = true,
         } = supplementary;
+
+        switch (true) {
+            case Array.isArray(points) && points.length === 4 && points.every(p => Number.isFinite(p.x) && Number.isFinite(p.y)):
+                this.#setCoordinatesFromPoints(points, shiftCoordinate);
+                break;
+            case Object.keys(lengths).length == 2:
+                if(('lengthAB' in lengths || 'lengthBC' in lengths) && ('lengthCD' in lengths || 'lengthDA' in lengths))
+                    throw new Error(`Two parallel sides of a parallelogram are given. ${JSON.stringify(lengths)}`);
+
+                if (!angles.angle || typeof angles.angle !== 'object' || Object.keys(angles.angle).length === 0)
+                    throw new TypeError(`No angle of the parallelogram is defined. ${JSON.stringify(angles)}`);
+                
+                this.#setAngles(angles);
+                this.#setCoordinates(lengths, shiftCoordinate);
+                break;
+            default:
+               throw new TypeError(`Invalid arguments: Received points ${JSON.stringify(points)}, lengths ${JSON.stringify(lengths)}. Please provide either four Point or two side lengths`);
+        }
+        
+        this.addFace(this._vertices);
 
         if(calculateDiagonals){
             this._setDiagonals();
@@ -72,7 +81,7 @@ export default class Parallelogram extends Quadrilateral {
         }
     }
 
-    #setCoordinates(lengths) {
+    #setCoordinates(lengths, shiftCoordinate) {
         let sideAB, sideBC;
 
         Object.keys(lengths).forEach(key => {
@@ -104,9 +113,73 @@ export default class Parallelogram extends Quadrilateral {
         const C = new Point(sideAB + x, y);
         const D = new Point(x, y);
 
-        let point = new Segment(A,C).intersect(new Segment(B,D))[0];
+        const center = new Segment(A,C).intersect(new Segment(B,D))[0];
 
-        [this._pointA, this._pointB, this._pointC, this._pointD] = [A, B, C, D].map((vertex) => shiftCoordinate2D(vertex, point));
+        if (shiftCoordinate) {
+            [this._pointA, this._pointB, this._pointC, this._pointD] = [A, B, C, D].map((vertex) => shiftCoordinate2D(vertex, center));
+        } else {
+            [this._pointA, this._pointB, this._pointC, this._pointD] = [A, B, C, D];
+        }
+        
+        this._vertices = [this._pointA, this._pointB, this._pointC, this._pointD];
+    }
+
+    #setCoordinatesFromPoints(points, shiftCoordinate) {
+        const eps = 1e-9;
+        const pts = points.map(p => (p instanceof Point ? p : new Point(p.x, p.y)));
+        if (pts.length !== 4) {
+            throw new TypeError("Expected 4 points to define a parallelogram");
+        }
+
+        const [A, B, C, D] = pts;
+
+        const AB = new Segment(A, B);
+        const BC = new Segment(B, C);
+        const CD = new Segment(C, D);
+        const DA = new Segment(D, A);
+
+        if (!isValidQuadrilateral(AB.length, BC.length, CD.length, DA.length)) {
+            throw new Error(
+                "Provided points do not form a valid quadrilateral: side-length constraints not satisfied"
+            );
+        }
+        
+        let angleDAB = new Angle(D, A, B).angleInRadians;
+        let angleABC = new Angle(A, B, C).angleInRadians;
+        let angleBCD = new Angle(B, C, D).angleInRadians;
+        let angleCDA = new Angle(C, D, A).angleInRadians;
+        
+        if (
+            Math.abs(angleABC - angleCDA) > eps ||
+            Math.abs(angleBCD - angleDAB) > eps
+        ) {
+            throw new Error(
+                "Provided points do not form a parallelogram: opposite sides are not parallel"
+            );
+        }
+        if (Math.abs(AB.length - CD.length) > eps || Math.abs(BC.length - DA.length) > eps) {
+            throw new Error(
+                "Provided points do not form a parallelogram: opposite sides are not equal"
+            );
+        }
+
+        // Set angles from points
+        this._angleAInRadians = angleDAB;
+        this._angleBInRadians = angleABC;
+        this._angleCInRadians = angleBCD;
+        this._angleDInRadians = angleCDA;
+
+        // Center (intersection of diagonals)
+        const center = new Segment(A, C).intersect(new Segment(B, D))[0];
+
+        if (shiftCoordinate) {
+            [this._pointA, this._pointB, this._pointC, this._pointD] = pts.map(v =>
+                shiftCoordinate2D(v, center)
+            );
+        } else {
+            [this._pointA, this._pointB, this._pointC, this._pointD] = pts;
+        }
+
         this._vertices = [this._pointA, this._pointB, this._pointC, this._pointD];
     }
 }
